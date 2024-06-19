c5fe38c33fc3a2666e1a4c727d0a47cbd4db83fe
Nikita Pastukhov <pastukhovns@yandex.ru>
2024-06-19 17:41:05 +0500

    Completed

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -1,38 +1,22 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarServiceImp;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
 
-
-//    (@RequestParam(value = "count", defaultValue = "5"))
+    @Autowired
+    private CarServiceImp carServiceImp;
 
     @GetMapping("/cars")
-    public String showCar(@RequestParam(value = "count", defaultValue = "5")  int count, Model model) {
-        System.out.println();
-        List<Car> carList = new ArrayList<>();
-        carList.add(new Car("Lada", "Lada", 1970));
-        carList.add(new Car("BMW", "Bayerische Motoren Werke", 1916));
-        carList.add(new Car("Toyota", "Toyota Motor Corporation", 1937));
-        carList.add(new Car("Audi", "Audi", 1909));
-        carList.add(new Car("UAZ", "Ulyanovsk Automobile Plant", 1941));
-
-        model.addAttribute("tableCar", carList.stream().limit(count));
-
-
-//        if (count == 5) {
-//            System.out.println(carList);
-//        } else
-
-
+    public String showCar(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("tableCar", carServiceImp.getCar(count));
         return "cars";
     }
 }
